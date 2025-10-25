87747b031e5ea6782bbafb027fc91ca159c6e9cd
surajsikha219-dev <surajsikha219@gmail.com>
2025-10-25 19:03:40 +0530

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
